0f2ca79953380ac5330375d214fbf0b9f4a534e1
gohansaem <gohansaem@gmail.com>
2024-07-29 17:52:23 +0900

    ✨ comment text-box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import CommentList from '@/components/common/Comment';
+
 export default function Home() {
-  return <div>랜딩페이지</div>;
+  return (
+    <>
+      <div>랜딩페이지</div>
+      <CommentList />
+    </>
+  );
 }
