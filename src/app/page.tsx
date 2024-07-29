11e73b02b91907499abb132b6e6175f978a39495
yongin <rladls6412@naver.com>
2024-07-29 19:28:07 +0900

    🎨 모달창 디자인 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Modal } from '@/components/common/Modal';
+
 export default function Home() {
-  return <div>랜딩페이지</div>;
+  return (
+    <div>
+      랜딩페이지
+      <Modal message="댓글을 삭제하시겠어요?" />
+    </div>
+  );
 }
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,3 +1,25 @@
-const Modal = () => {
-  return <div>모달창</div>;
+interface ModalProps {
+  message: string;
+}
+
+export const Modal: React.FC<ModalProps> = ({ message }) => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-black/60 text-white">
+      <div className="w-96 rounded-[12px] bg-white px-[38px] py-[40px] text-center">
+        <div>
+          <h1 className="mb-[40px] text-2xl font-semibold text-black">
+            {message}
+          </h1>
+          <div className="flex items-center justify-center gap-4">
+            <button className="w-full rounded-xl bg-[#ECEFF4] p-4 text-xl text-black">
+              취소
+            </button>
+            <button className="w-full rounded-xl bg-[#FF6577] p-4 text-xl text-white">
+              삭제하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
