e3a484b9d7eb7de5071fb340310ddcf645cc9472
gohansaem <gohansaem@gmail.com>
2024-07-30 14:18:08 +0900

    ♻️ 테일윈드 조정

--- src/components/common/SearchText.tsx
+++ src/components/common/SearchText.tsx
@@ -9,27 +9,24 @@ interface SearchInputProps {
 
 const SearchText: React.FC<SearchInputProps> = ({ value, onChange }) => {
   return (
-    <div className="w-[50%]">
-      {/* 부모 컨테이너 부분이고 임시로 만듬 */}
-      <div className="relative w-full">
-        <input
-          value={value}
-          onChange={(event) => onChange(event.target.value)}
-          className="h-[52px] w-full border-0 border-b-2 py-2 pr-5 text-base focus:outline-none md:h-[60px] md:text-xl lg:h-[80px] lg:pr-8 lg:text-2xl"
-          style={{ borderBottomColor: '#40516E' }}
+    <div className="relative w-full">
+      <input
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+        className="h-[52px] w-full border-0 border-b-2 border-blue-800 py-2 pr-5 text-xl text-black-700 focus:outline-none md:h-[60px] md:text-2xl xl:h-[80px] xl:border-b-4 xl:pr-8 xl:text-[24px]"
+      />
+      <div className="absolute right-0 top-1/2 -translate-y-1/2 transform">
+        <Image
+          src="/assets/icons/search_icon.svg" 
+          alt="Search Icon"
+          width={20}
+          height={20}
+          className="h-5 w-5 md:h-5 md:w-5 xl:h-9 xl:w-9"
         />
-        <div className="absolute right-0 top-1/2 -translate-y-1/2 transform">
-          <Image
-            src="/assets/icons/search_icon.svg"
-            alt="Search Icon"
-            width={20}
-            height={20}
-            className="h-5 w-5 md:h-5 md:w-5 lg:h-9 lg:w-9"
-          />
-        </div>
       </div>
     </div>
   );
 };
 
 export default SearchText;
+    
