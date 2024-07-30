cc6cf887c7b6737ec2278cc0d3d37cf99e3eb3ad
gohansaem <gohansaem@gmail.com>
2024-07-30 14:43:57 +0900

    🔥 불필요 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import SearchText from '@/components/common/SearchText';
-
 export default function Home() {
-  return (
-    <>
-      <div>랜딩페이지</div>
-      <SearchText /> {/* 테스트용 */}
-    </>
-  );
+  return <div>랜딩페이지</div>;
 }
