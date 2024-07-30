77069d1915cecd64e9658244acbd46e13b4e49fb
yongin <rladls6412@naver.com>
2024-07-30 16:42:58 +0900

    테스트중

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,22 +1,53 @@
-interface ModalProps {
-  message: string;
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
 }
 
-export const Modal: React.FC<ModalProps> = ({ message }) => {
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  message: string;
+  buttons: ButtonProps[];
+}
+
+/**
+ * @isOpen - 모달의 표시여부를 나타냄
+ * @onClose - 모달을 닫는 함수로 배경 클릭하면 호출됨
+ * @message - 모달창에 들어갈 메시지
+ * @buttons - 모달창에 들어갈 버튼
+ */
+
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  message,
+  buttons,
+}) => {
+  if (!isOpen) return null;
+
   return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-black/60 text-white">
-      <div className="w-96 rounded-[12px] bg-white px-[38px] py-[40px] text-center">
+    <div
+      className="fixed bottom-0 left-0 right-0 top-0 z-50 flex items-center justify-center bg-black/60 text-white"
+      onClick={onClose}
+    >
+      <div
+        className="w-96 rounded-[12px] bg-white px-[38px] py-[40px] text-center"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div>
           <h1 className="mb-[40px] text-2xl font-semibold text-black">
             {message}
           </h1>
           <div className="flex items-center justify-center gap-4">
-            <button className="w-full rounded-xl bg-[#ECEFF4] p-4 text-xl text-black">
-              취소
-            </button>
-            <button className="w-full rounded-xl bg-[#FF6577] p-4 text-xl text-white">
-              삭제하기
-            </button>
+            {buttons.map((button, index) => (
+              <button
+                className="w-full rounded-[12px] bg-slate-800 p-4 text-xl text-orange-600"
+                key={index}
+                onClick={button.onClick}
+              >
+                {button.text}
+              </button>
+            ))}
           </div>
         </div>
       </div>
