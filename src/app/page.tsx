18e45eb30f9bc13d7a72891f6f32cc9e16d611b9
gohansaem <gohansaem@gmail.com>
2024-08-01 14:40:44 +0900

    ✨ 메뉴이미지 클릭시 사이드창 UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Header, HeaderOnlyLogo } from '@/components/common/Header';
 export default function Home() {
   return (
     <div>
-      <HeaderOnlyLogo />
       <Header />
     </div>
   );
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,8 @@
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const HeaderOnlyLogo = () => {
   return (
@@ -19,50 +23,88 @@ const HeaderOnlyLogo = () => {
 };
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="xl:px:[120px] flex h-[52px] items-center border border-blue-900 px-[24px] md:h-[60px] md:px-[72px] xl:h-[80px]">
-      <div className="container mx-auto flex items-center justify-between">
-        {/* 메뉴 아이콘 - 모바일에서만 표시 */}
-        <div className="flex gap-3 md:gap-6 xl:gap-9">
-          <div className="md:hidden">
-            <Image
-              src="/assets/icons/gnb_menu.svg"
-              alt="Menu"
-              width={24}
-              height={24}
-              className="h-[24px] w-[24px]"
-            />
+    <>
+      <nav className="flex h-[52px] items-center border border-blue-900 px-[24px] md:h-[60px] md:px-[72px] xl:h-[80px] xl:px-[120px]">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="flex gap-3 md:gap-6 xl:gap-9">
+            <div className="md:hidden" onClick={toggleMenu}>
+              <Image
+                src="/assets/icons/gnb_menu.svg"
+                alt="Menu"
+                width={24}
+                height={24}
+                className="h-[24px] w-[24px]"
+              />
+            </div>
+
+            <div>
+              <Image
+                src="/assets/icons/logo_home.svg"
+                alt="Home Logo"
+                width={101}
+                height={26}
+                className="h-[26px] w-[101px] xl:h-[36px] xl:w-[131px]"
+              />
+            </div>
+
+            <div className="hidden items-center gap-6 md:flex xl:gap-9">
+              <Link href="#" className="text-lg text-black-600 xl:text-xl">
+                피드
+              </Link>
+              <Link href="#" className="text-lg text-black-600 xl:text-xl">
+                검색
+              </Link>
+            </div>
           </div>
 
-          <div>
+          <div className="flex items-center gap-[6px]">
             <Image
-              src="/assets/icons/logo_home.svg"
-              alt="Home Logo"
-              width={101}
-              height={26}
-              className="h-[26px] w-[101px] xl:h-[36px] xl:w-[131px]"
+              src="/assets/icons/default_user.svg"
+              alt="User Profile"
+              width={16}
+              height={16}
+              className="h-[16px] w-[16px] xl:h-[24px] xl:w-[24px]"
             />
-          </div>
-
-          <div className="hidden items-center gap-6 md:flex xl:gap-9">
-            <button className="text-lg text-black-600 xl:text-xl">피드</button>
-            <button className="text-lg text-black-600 xl:text-xl">검색</button>
+            <p className="text-md text-gray-300 xl:text-lg">김코드</p>
           </div>
         </div>
+      </nav>
 
-        {/* 사용자 프로필 */}
-        <div className="flex items-center gap-[6px]">
-          <Image
-            src="/assets/icons/default_user.svg"
-            alt="User Profile"
-            width={16}
-            height={16}
-            className="h-[16px] w-[16px] xl:h-[24px] xl:w-[24px]"
-          />
-          <p className="text-md text-gray-300 xl:text-lg">김코드</p>
-        </div>
-      </div>
-    </nav>
+      {isMenuOpen && (
+        <>
+          <div
+            className="fixed inset-0 z-40 bg-[#00000099]"
+            onClick={toggleMenu}
+          ></div>
+          <div className="fixed inset-y-0 left-0 z-50 w-[220px] bg-white shadow-lg">
+            <div className="border-bottom border-line-100 m-4 flex items-center justify-end">
+              <button onClick={toggleMenu} className="text-xl">
+                X
+              </button>
+            </div>
+            <ul>
+              <li className="px-5 py-6">
+                <Link href="#" className="text-xl text-black-600">
+                  피드
+                </Link>
+              </li>
+              <li className="px-5 py-6">
+                <Link href="#" className="text-xl text-black-600">
+                  검색
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
