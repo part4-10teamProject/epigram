66887081c28dff01c6e244c2f4a3c4306081b764
YUKIJOO <koreala1@naver.com>
2024-08-03 21:17:20 +0900

    ⚡️ textareasolid 500자 글자 수 제한 및 경고 메시지 작동 수정!

--- src/components/common/textareaSolid.tsx
+++ src/components/common/textareaSolid.tsx
@@ -1,22 +1,33 @@
 'use client';
 
+import { useState } from 'react';
+
 function TextAreaSolid() {
+  const [text, setText] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
   return (
-    <form className="group relative">
+    <label className="">
       <textarea
+        name="에피그램 내용 작성"
         typeof="text"
+        value={text}
         id="내용"
         maxLength={500}
-        rows={15}
+        onChange={handleChange}
         placeholder="500자 이내로 입력해주세요"
-        onFocus={() => console.log('본문을 작성해주세요')}
-        className="h-[132px] w-[312px] border pl-2 pt-2 text-xl font-normal leading-[26px] group-hover:border-red-500 group-active:border-red-500 md:h-[132px] md:w-[384px] xl:h-[148px] xl:w-[640px]"
+        className="resize: none h-[132px] w-[312px] border pl-2 pt-2 text-xl font-normal leading-[26px] group-hover:border-red-500 group-active:border-red-500 md:h-[132px] md:w-[384px] xl:h-[148px] xl:w-[640px]"
       ></textarea>
 
-      <p className="invisible mt-2 w-[312px] text-right text-sm text-pink-600 group-hover:visible group-active:visible md:h-[132px] md:w-[384px] xl:h-[148px] xl:w-[640px]">
+      <p
+        className={`${text.length >= 500 ? 'block' : 'hidden'} mt-2 w-[312px] text-right text-sm text-pink-600 md:h-[132px] md:w-[384px] xl:h-[148px] xl:w-[640px]`}
+      >
         500자 이내로 입력해주세요.
       </p>
-    </form>
+    </label>
   );
 }
 
