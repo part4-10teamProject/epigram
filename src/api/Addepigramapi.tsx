ec718e158f819c5253002a2fa439f6f8c675f12a
YUKIJOO <koreala1@naver.com>
2024-08-17 13:09:43 +0900

    ✨ 에피그램 만들기 페이지 api 생성 및 cookie 라이브러리 생성

---
+++ src/api/Addepigramapi.tsx
@@ -0,0 +1,25 @@
+import { instance } from './AxiosInstance';
+import Cookies from 'js-cookie';
+
+export interface Addepigram {
+  tag: string[];
+  referenceUrl: string;
+  referenceTitle: string;
+  author: string;
+  content: string;
+}
+
+export const AddEpigramPost = async (epigram: Addepigram) => {
+  try {
+    const token = Cookies.get('token');
+
+    const response = await instance.post('/epigrams', epigram, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('에피그램이 등록되지 않았습니다!');
+  }
+};
