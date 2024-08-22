45dc2bbcebfb9785fbfd62ae9bdfa18a321e1399
gohansaem <gohansaem@gmail.com>
2024-08-22 17:51:11 +0900

    🐛 중복 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import ClientHeader from '@/components/common/Header';
 import { QueryProvider } from '@/contexts/QueryProvider';
 import type { Metadata } from 'next';
 import './globals.css';
-import { QueryProvider } from '@/contexts/QueryProvider';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import ClientHeader from '@/components/common/Header';
 import { AuthProvider } from '@/contexts/AuthContext';
 
 export const metadata: Metadata = {
